b7f441c8b56d3715a10ce24cbd488ea5dca42218
Craig Lymus <craiglymus@gmail.com>
2018-09-29 21:57:34 -0700

    added some HTML and JS

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log('Hello');
+// console.log('Hello');
+
+var menuButton = document.querySelector('i');
+var hamburger = document.getElementById('hamburger')
+var navbarElements = document.querySelector('.nav-list')
+// Menu Click Event Listener
+
+menuButton.addEventListener('click', handleMenuClick);
+
+// Handle Menu Click 
+
+function handleMenuClick () {
+	// console.log('Click!');
+	var navbar = document.querySelector('.nav-list');
+	
+	if (navbar.classList.contains('navbar-hidden')) {
+		navbar.classList.toggle('navbar-hidden');
+		menuButton.style.color = '#fff';
+		menuButton.classList.remove('fa-bars'); 
+		menuButton.classList.add('fa-times');
+	} else {
+		navbar.classList.toggle('navbar-hidden');
+		menuButton.style.color = '#000'
+		menuButton.classList.remove('fa-times')
+		menuButton.classList.add('fa-bars');
+	}
+}
+
+
+hamburger.addEventListener('click', showHiddenNav);
+
+
+function showHiddenNav() {
+	console.log('hellooo')
+	navbarElements.style.toggle('navbar')
+}
